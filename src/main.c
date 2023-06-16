1aed25f3d522fd1791a054b1f202b487e0b37635
Gowtham Krishnan <gowtham@neeveetech.com>
2023-06-16 23:42:35 +0530

    initial commite

---
+++ src/main.c
@@ -0,0 +1,214 @@
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <soc.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+
+#include <bluetooth/services/lbs.h>
+
+#include <zephyr/settings/settings.h>
+
+
+#include "ble_char.h"
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+
+
+
+int cTemp;
+int ble_temp ;
+int humidity;
+int ble_hum ;
+int timer_val=10;
+
+static bool app_button_state;
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_LBS_VAL),
+};
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err)
+	{
+		printk("Connection failed (err %u)\n", err);
+		return;
+	}
+
+	printk("Connected\n");
+
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected (reason %u)\n", reason);
+
+}
+
+#ifdef CONFIG_BT_LBS_SECURITY_ENABLED
+static void security_changed(struct bt_conn *conn, bt_security_t level,
+							 enum bt_security_err err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (!err)
+	{
+		printk("Security changed: %s level %u\n", addr, level);
+	}
+	else
+	{
+		printk("Security failed: %s level %u err %d\n", addr, level,
+			   err);
+	}
+}
+#endif
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+#ifdef CONFIG_BT_LBS_SECURITY_ENABLED
+	.security_changed = security_changed,
+#endif
+};
+
+#if defined(CONFIG_BT_LBS_SECURITY_ENABLED)
+static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Passkey for %s: %06u\n", addr, passkey);
+}
+
+static void auth_cancel(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing cancelled: %s\n", addr);
+}
+
+static void pairing_complete(struct bt_conn *conn, bool bonded)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing completed: %s, bonded: %d\n", addr, bonded);
+}
+
+static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing failed conn: %s, reason %d\n", addr, reason);
+}
+
+static struct bt_conn_auth_cb conn_auth_callbacks = {
+	.passkey_display = auth_passkey_display,
+	.cancel = auth_cancel,
+};
+
+static struct bt_conn_auth_info_cb conn_auth_info_callbacks = {
+	.pairing_complete = pairing_complete,
+	.pairing_failed = pairing_failed};
+#else
+static struct bt_conn_auth_cb conn_auth_callbacks;
+static struct bt_conn_auth_info_cb conn_auth_info_callbacks;
+#endif
+
+
+void tcp_work_handler(struct k_work *work)
+{
+	/* do the processing that needs to be done periodically */
+	si7006();
+	ble_temp = cTemp * 100;
+	ble_hum = humidity * 100;
+	hum_notify();
+	temp_notify();
+}
+
+K_WORK_DEFINE(tcp_work, tcp_work_handler);
+void tcp_timer_handler(struct k_timer *dummy)
+{
+	k_work_submit(&tcp_work);
+}
+
+struct k_timer tcp_timer;
+
+void main(void)
+{
+	int err;
+
+	printk("Starting Bluetooth Peripheral LBS example\n");
+
+
+	if (IS_ENABLED(CONFIG_BT_LBS_SECURITY_ENABLED))
+	{
+		err = bt_conn_auth_cb_register(&conn_auth_callbacks);
+		if (err)
+		{
+			printk("Failed to register authorization callbacks.\n");
+			return;
+		}
+
+		err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
+		if (err)
+		{
+			printk("Failed to register authorization info callbacks.\n");
+			return;
+		}
+	}
+
+	err = bt_enable(NULL);
+	if (err)
+	{
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	if (IS_ENABLED(CONFIG_SETTINGS))
+	{
+		settings_load();
+	}
+
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad),
+						  sd, ARRAY_SIZE(sd));
+	if (err)
+	{
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+
+	// timer
+	k_timer_init(&tcp_timer, tcp_timer_handler, NULL);
+	k_timer_start(&tcp_timer, K_SECONDS(0), K_SECONDS(timer_val));
+}
